ac6aae71e85ca41ee5fc3424689c742e9cd18042
j7an <jiatan24@gmail.com>
2016-09-20 20:13:35 +0000

    comment optional packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,37 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 const eslint = require('gulp-eslint');
-const runSequence = require('run-sequence');
-const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
-const gulpStylelint = require('gulp-stylelint');
+// const runSequence = require('run-sequence');
+// const postcss = require('gulp-postcss');
+// const autoprefixer = require('autoprefixer');
+// const gulpStylelint = require('gulp-stylelint');
 
-gulp.task('css', function(callback) {
-  runSequence('postcss', 'lint-css', callback);
-});
+// gulp.task('css', function(callback) {
+//   runSequence('postcss', 'lint-css', callback);
+// });
 
-gulp.task('postcss', function() {
-  var processors = [
-    autoprefixer
-  ];
+// gulp.task('postcss', function() {
+//   var processors = [
+//     autoprefixer
+//   ];
 
-  gulp.src("projects/**/*.css")
-    .pipe(postcss(processors))
-    .pipe(gulp.dest("babel/projects"));
-  gulp.src("exercises/**/*.css")
-    .pipe(postcss(processors))
-    .pipe(gulp.dest("babel/exercises"));
-  return;
-});
+//   gulp.src("projects/**/*.css")
+//     .pipe(postcss(processors))
+//     .pipe(gulp.dest("babel/projects"));
+//   gulp.src("exercises/**/*.css")
+//     .pipe(postcss(processors))
+//     .pipe(gulp.dest("babel/exercises"));
+//   return;
+// });
 
-gulp.task('lint-css', function() {
-  return gulp.src(["projects/**/*.css", "exercises/**/*.css"])
-    .pipe(gulpStylelint({
-      reporters: [
-        {formatter: "string", console: true}
-      ]
-    }));
-});
+// gulp.task('lint-css', function() {
+//   return gulp.src(["projects/**/*.css", "exercises/**/*.css"])
+//     .pipe(gulpStylelint({
+//       reporters: [
+//         {formatter: "string", console: true}
+//       ]
+//     }));
+// });
 
 gulp.task('default', function() {
   // Run ESLint
