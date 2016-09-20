80f8219c70dadc1c616bcd7f89f8754742fb1a4d
j7an <jiatan24@gmail.com>
2016-09-20 22:18:14 +0000

    add pig latin

---
+++ exercises/IntermediateAlgorithmScripting/06-PigLatin/Pig-Latin.js
@@ -0,0 +1,38 @@
+/* Translate the provided string to pig latin.
+
+Pig Latin takes the first consonant (or consonant cluster) of an English word,
+moves it to the end of the word and suffixes an "ay".
+
+If a word begins with a vowel you just add "way" to the end.
+
+Input strings are guaranteed to be English words in all lowercase.
+*/
+
+function translatePigLatin(str) {
+  var vowel = "aeiou";
+  var match = -1;
+  var index = -1;
+
+  // find index of first vowel
+  for (var x = 0; x < str.length && match < 0; x++) {
+    match = vowel.indexOf(str.charAt(x));
+    if (match >= 0) {
+      index = x;
+    }
+  }
+  
+  if (index > 0) {
+    str = str.slice(index) + str.substring(0, index) + "ay";
+  } else {
+    str = str.slice(index) + str.substring(0, index) + "way";
+  }
+
+  return str;
+}
+
+console.log(translatePigLatin("consonant"));
+console.log(translatePigLatin("california"));
+console.log(translatePigLatin("paragraphs"));
+console.log(translatePigLatin("glove"));
+console.log(translatePigLatin("algorithm"));
+console.log(translatePigLatin("eight"));
