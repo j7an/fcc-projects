005944cf8b1b4333f8858d523a128d692a5436db
Jia Tan <j7an@users.noreply.github.com>
2017-06-09 16:47:19 -0700

    FindersKeepers

---
+++ exercises/IntermediateAlgorithmScripting/16-FindersKeepers/FindersKeepers.js
@@ -0,0 +1,33 @@
+function findElement(arr, func) {
+//   let num = 0;
+//   return num;
+    console.log(arr.find(func))
+    return arr.find(func);
+}
+
+// function findElement(arr, func) {
+// //   let num = 0;
+// //   return num;
+//     console.log(arr.filter(func)[0])
+//     return arr.filter(func)[0];
+// }
+
+// function findElement(arr, func) {
+// //   let num;
+// //   return num;
+//     for (let i = 0; i < arr.length; i++) {
+//         if (func(arr[i])) {
+//             console.log(arr[i]);
+//             return arr[i];
+//         }
+//     }
+
+//     return undefined;
+// }
+
+// findElement([1, 2, 3, 4], function(num){ return num % 2 === 0; });
+findElement([1, 2, 3, 4], num => num % 2 === 0);
+// findElement([1, 3, 5, 8, 9, 10], function(num) { return num % 2 === 0; }); // 8
+findElement([1, 3, 5, 8, 9, 10], num => num % 2 === 0);
+// findElement([1, 3, 5, 9], function(num) { return num % 2 === 0; }); // undefined
+findElement([1, 3, 5, 9], num => num % 2 === 0);
