d153519ba662cd4ce4325b3c80aec80030c57e54
Jia Tan <j7an@users.noreply.github.com>
2017-06-09 17:00:19 -0700

    simplify

--- exercises/IntermediateAlgorithmScripting/16-FindersKeepers/FindersKeepers.js
+++ exercises/IntermediateAlgorithmScripting/16-FindersKeepers/FindersKeepers.js
@@ -3,12 +3,12 @@ Create a function that looks through an array (first argument) and returns
 the first element in the array that passes a truth test (second argument).
 */
 
-function findElement(arr, func) {
-//   let num = 0;
-//   return num;
-    console.log(arr.find(func))
-    return arr.find(func);
-}
+// function findElement(arr, func) {
+// //   let num = 0;
+// //   return num;
+//     console.log(arr.find(func))
+//     return arr.find(func);
+// }
 
 // function findElement(arr, func) {
 // //   let num = 0;
@@ -17,18 +17,18 @@ function findElement(arr, func) {
 //     return arr.filter(func)[0];
 // }
 
-// function findElement(arr, func) {
-// //   let num;
-// //   return num;
-//     for (let i = 0; i < arr.length; i++) {
-//         if (func(arr[i])) {
-//             console.log(arr[i]);
-//             return arr[i];
-//         }
-//     }
+function findElement(arr, func) {
+  let num;
+//   return num;
+    for (let i = 0; i < arr.length; i++) {
+        if (func(arr[i])) {
+            console.log(arr[i]);
+            return arr[i];
+        }
+    }
 
-//     return undefined;
-// }
+    return num;
+}
 
 // findElement([1, 2, 3, 4], function(num){ return num % 2 === 0; });
 findElement([1, 2, 3, 4], num => num % 2 === 0);
