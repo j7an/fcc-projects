6700bf4c38a647512f2951271258613bf804ea9c
Jia Tan <j7an@users.noreply.github.com>
2017-06-20 11:16:09 -0700

    EverythingBeTrue

---
+++ exercises/IntermediateAlgorithmScripting/20-EverythingBeTrue/EverythingBeTrue.js
@@ -0,0 +1,49 @@
+/*
+Check if the predicate (second argument) is truthy on all elements of a collection (first argument).
+
+Remember, you can access object properties through either dot notation or [] notation.
+*/
+
+function truthCheck(collection, pre) {
+    // Is everyone being true?
+    //   console.log(collection);
+    // collection.forEach(function(element) {
+    //     console.log(element);
+    //     console.log(element.sex);
+    //     console.log(element[pre]);
+    //     if (element[pre]) {
+    //         console.log(true);
+    //     } else {
+    //         console.log(false);
+    //     }
+    // })
+    var status;
+    for (var i = 0; i < collection.length; i++) {
+        // console.log(collection[i]);
+        if (collection[i][pre]) {
+            // console.log(collection[i]);
+            // console.log(true);
+            status = true;
+        } else {
+            // console.log(collection[i]);
+            // console.log(false);
+            status = false;
+            break;
+        }
+    }
+    // console.log(pre);
+    // return pre;
+    console.log(collection);
+    console.log(status);
+    return status;
+}
+
+truthCheck([{ "user": "Tinky-Winky", "sex": "male" }, { "user": "Dipsy", "sex": "male" }, { "user": "Laa-Laa", "sex": "female" }, { "user": "Po", "sex": "female" }], "sex");
+truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
+truthCheck([{"user": "Tinky-Winky", "sex": "male", "age": 0}, {"user": "Dipsy", "sex": "male", "age": 3}, {"user": "Laa-Laa", "sex": "female", "age": 5}, {"user": "Po", "sex": "female", "age": 4}], "age");
+truthCheck([{"name": "Pete", "onBoat": true}, {"name": "Repeat", "onBoat": true}, {"name": "FastFoward", "onBoat": null}], "onBoat");
+truthCheck([{"name": "Pete", "onBoat": true}, {"name": "Repeat", "onBoat": true, "alias": "Repete"}, {"name": "FastFoward", "onBoat": true}], "onBoat");
+truthCheck([{"single": "yes"}], "single");
+truthCheck([{"single": ""}, {"single": "double"}], "single");
+truthCheck([{"single": "double"}, {"single": undefined}], "single");
+truthCheck([{"single": "double"}, {"single": NaN}], "single");
